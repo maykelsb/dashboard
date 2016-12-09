e9b10624c8d1da7a4d31ca52ebd20ffe5c2935ce
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-12-09 20:13:36 +0000

    Twig template

--- web/index.php
+++ web/index.php
@@ -8,7 +8,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), [
-    'twig.path' => __DIR__ . '/views'
+    'twig.path' => __DIR__ . '/src/views',
+    'twig.templates' => ['bootstrap_3_layout.html.twig']
 ]);
 
 $app->get('/', function(){
