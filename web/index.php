eac4e29897c79c3f6e6bde55282456ad35529943
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-12-09 19:23:08 +0000

    Adding twig to the project

--- web/index.php
+++ web/index.php
@@ -1,11 +1,16 @@
 <?php
 /**
+ * Dashboard of Doom
  *
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
-//
+
 $app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+    'twig.path' => __DIR__ . '/views'
+]);
+
 $app->get('/', function(){
     return 'Home!';
 });
